5c9ba851f8710164e3acb1f801bf8307b1424184
Andrey Lukyanchuk <andreylukyanchuk623@gmail.com>
2020-06-27 17:27:52 +0300

    reworked send_beat func, fixed bugs

--- orders/tasks.py
+++ orders/tasks.py
@@ -7,22 +7,13 @@ def send_beat(order_id):
     order_item = OrderItem.objects.filter(order=order)
     subject = f"Заказ номер{order_id}"
     message = ""
-    file_path = ""
+    file_path = []
     for item in order_item:
         name = item.beat.name
         file = item.beat.file.path
         message += f"{name} \n"
-        file_path += file
-        yield file_path
+        file_path.append(file)
     email = EmailMessage(subject, message, "fred@example.com", [order.email])
-    for _ in range(1, order_item.count() + 1):  # отправлять файлов столько сколько попадает в order_item
-        email.attach_file(file_path)
-    # email.attach_file(file_path)
+    for index in range(0, len(file_path)):
+        email.attach_file(file_path[index])
     email.send(fail_silently=False)
-
-    # mail_sent = send_mail(subject,
-    #                       message,
-    #                       "andreypoelsup@gmail.com",
-    #                       [order.email],
-    #                       )
-    # return mail_sent
